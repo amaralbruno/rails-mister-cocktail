ae52ae4c832128515337589720d0c0db79995a4d
Bruno Amaral <bru.amaral88@gmail.com>
2017-08-03 12:20:21 -0300

    cocktails controller done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,49 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
